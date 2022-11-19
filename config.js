9390de623a26abce6ea7c77112c2ab6b1c66ce5b
Cuongnobro <letrongcuong172005@gmail.com>
2022-11-19 21:54:17 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thầy Cô Giáo';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Chúc Mừng Thầy Cô Giáo 20/11';
+const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, chúng em xin gửi đến thầy cô những lời cảm ơn chân thành nhất vì đã dìu dắt chúng em đến gần hơn với chân trời kiến thức. Kính chúc thầy cô luôn luôn mạnh khỏe và hạnh phúc để luôn mang đến cho chúng em ngày càng nhiều bài học hay và bổ ích.';
+
+//Phiên bản Của LTC xin ghi nguồn
+const giftImage = '112.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
