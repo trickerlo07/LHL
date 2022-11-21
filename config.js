a8cf6eec5b7010e57dbe0e16f92c97362ba0a38a
trickerlo07 <118740941+trickerlo07@users.noreply.github.com>
2022-11-21 20:36:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const nameGirl = 'Thầy Cô Giáo';
-const giftUrl = 'http://nodemy.vn';
+const nameGirl = 'Bạn Quý cuti';
+const giftUrl = 'https://trickerlo07.github.io/tym/';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Happy Birthday 22-11';
 const contentCard = 'Hôm nay 22-11 cũng là sinh nhật bà á. Tui cũng muốn đến trực tiếp tặng quà lắm nhưng mà chắc điều kiện không cho phép. Tui xin code bức thư này như một món quà, mong cậu nhận. Chúc cậu có một cái sinh nhật thiệc đặc biệt, một ngày luôn hạnh phúc và vui vẻ bên gia đình và người thân. A1 cơ mà với thủ khoa nữa chắc là học nhiều lém, nhớ giữ gìn sức khỏe nhó.';
