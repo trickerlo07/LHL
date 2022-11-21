901d28cecfbdd7999278a4c9d838e8eb28509b66
trickerlo07 <118740941+trickerlo07@users.noreply.github.com>
2022-11-21 20:30:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Thầy Cô Giáo';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-11';
-const titleCard = 'Chúc Mừng Thầy Cô Giáo 20/11';
-const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, chúng em xin gửi đến thầy cô những lời cảm ơn chân thành nhất vì đã dìu dắt chúng em đến gần hơn với chân trời kiến thức. Kính chúc thầy cô luôn luôn mạnh khỏe và hạnh phúc để luôn mang đến cho chúng em ngày càng nhiều bài học hay và bổ ích.';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Happy Birthday 22-11';
+const contentCard = 'Hôm nay 22-11 cũng là sinh nhật bà á. Tui cũng muốn đến trực tiếp tặng quà lắm nhưng mà chắc điều kiện không cho phép. Tui xin code bức thư này như một món quà, mong cậu nhận. Chúc cậu có một cái sinh nhật thiệc đặc biệt, một ngày luôn hạnh phúc và vui vẻ bên gia đình và người thân. A1 cơ mà với thủ khoa nữa chắc là học nhiều lém, nhớ giữ gìn sức khỏe nhó.';
 
 //Phiên bản Của LTC xin ghi nguồn
 const giftImage = '112.jpg';
